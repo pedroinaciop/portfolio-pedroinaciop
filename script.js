ef5b032e517732c289dd58a8dbcfd39ed9e72b9a
Pedro Inácio Penha dos Santos <inaciopedro2004@gmail.com>
2023-06-30 16:58:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -20,88 +20,86 @@ function darkMode() {
     let email = contacts[1]
     let local = contacts[2]
 
-    if (checkBox.checked == true){
-        body.style.background = '#0f0f0f'  
-        header.style.background = '#0f0f0f'   
-        h1.style.color = 'white'   
-        h2.style.color = 'white' 
-        h3.style.color = 'white'  
-        title1.style.color = 'white'  
-        title2.style.color = 'white' 
-        title3.style.color = 'white' 
-        sub.style.color = 'white' 
-        tel.style.color = 'white' 
-        email.style.color = 'white' 
-        local.style.color = 'white' 
-        cel.style.color = 'white' 
+    if (checkBox.checked == true) {
+        body.style.background = '#0f0f0f'
+        header.style.background = '#0f0f0f'
+        h1.style.color = 'white'
+        h2.style.color = 'white'
+        h3.style.color = 'white'
+        title1.style.color = 'white'
+        title2.style.color = 'white'
+        title3.style.color = 'white'
+        sub.style.color = 'white'
+        tel.style.color = 'white'
+        email.style.color = 'white'
+        local.style.color = 'white'
+        cel.style.color = 'white'
         mail.style.color = 'white'
-        locale.style.color = 'white'  
+        locale.style.color = 'white'
     } else {
         body.style.background = 'white'
         header.style.background = 'white'
-        h1.style.color = 'black' 
-        h2.style.color = 'black'  
-        h3.style.color = 'black' 
-        title1.style.color = 'black'  
-        title2.style.color = 'black' 
+        h1.style.color = 'black'
+        h2.style.color = 'black'
+        h3.style.color = 'black'
+        title1.style.color = 'black'
+        title2.style.color = 'black'
         title3.style.color = 'black'
-        sub.style.color = 'black'  
-        tel.style.color = 'black' 
-        email.style.color = 'black' 
+        sub.style.color = 'black'
+        tel.style.color = 'black'
+        email.style.color = 'black'
         local.style.color = 'black'
-        locale.style.color = 'black'  
-        cel.style.color = 'black' 
+        locale.style.color = 'black'
+        cel.style.color = 'black'
         mail.style.color = 'black'
-         //Modo dark-mode da gambiarra, logo atualizo!
+        //Modo dark-mode da gambiarra, logo atualizo!
     }
-} 
+}
 
 function loading() {
     document.getElementsByClassName('box-load')[0].style.display = 'none'
     document.getElementsByClassName('content')[0].style.display = 'block'
     document.getElementsByClassName('btn')[0].style.display = 'none'
 }
- 
-document.addEventListener('DOMContentLoaded', function() {
+
+document.addEventListener('DOMContentLoaded', function () {
     let backToTopButton = document.getElementsByClassName('btn')[0]
 
-    window.addEventListener('scroll', function() {
-        if (window.scrollY > 160) { 
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > 160) {
             backToTopButton.style.display = 'block';
         } else {
             backToTopButton.style.display = 'none';
         }
     });
 
-     backToTopButton.addEventListener('click', function() {
+    backToTopButton.addEventListener('click', function () {
         scrollToTop(900);
     });
-      
+
     function scrollToTop(duration) {
         let start = document.documentElement.scrollTop || document.body.scrollTop;
         let startTime = Date.now();
-      
-    function scrollToTopAnimation() {
-        let currentTime = Date.now();
-        let elapsedTime = currentTime - startTime;
-        let scrollDuration = Math.min(duration, elapsedTime);
-      
-        document.documentElement.scrollTop = document.body.scrollTop = easeInOutCubic(scrollDuration, start, -start, duration);
-      
-        if (elapsedTime < duration) {
-            requestAnimationFrame(scrollToTopAnimation);
+
+        function scrollToTopAnimation() {
+            let currentTime = Date.now();
+            let elapsedTime = currentTime - startTime;
+            let scrollDuration = Math.min(duration, elapsedTime);
+
+            document.documentElement.scrollTop = document.body.scrollTop = easeInOutCubic(scrollDuration, start, -start, duration);
+
+            if (elapsedTime < duration) {
+                requestAnimationFrame(scrollToTopAnimation);
+            }
         }
-    }
-      
-    function easeInOutCubic(t, b, c, d) {
-        t /= d/2;
-        if (t < 1) return c/2*t*t*t + b;
-        t -= 2;
-        return c/2*(t*t*t + 2) + b;
-    }
-      
-    requestAnimationFrame(scrollToTopAnimation);
+
+        function easeInOutCubic(t, b, c, d) {
+            t /= d / 2;
+            if (t < 1) return c / 2 * t * t * t + b;
+            t -= 2;
+            return c / 2 * (t * t * t + 2) + b;
+        }
+
+        requestAnimationFrame(scrollToTopAnimation);
     }
 });
-
-
